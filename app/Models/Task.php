40f277eda4f33b94728c08aa0e5361de62f342f8
afghanelyas <afghan.elyas1@gmail.com>
2023-07-10 18:12:47 +0430

    create the function user which make many tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'priority',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
