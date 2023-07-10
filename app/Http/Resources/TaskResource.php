c5ecca5de57c0419773aa080c0e868721b13d12b
afghanelyas <afghan.elyas1@gmail.com>
2023-07-10 22:34:13 +0430

    creaate a task resouce

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => (string) $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'description' => $this->description,
+                'priority' => $this->priority,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ],
+            'relationship' => [
+                'id' => $this->user->id,
+                'user name' => $this->user->name,
+                'email' => $this->user->email,
+
+            ],
+
+        ];
+    }
+}
