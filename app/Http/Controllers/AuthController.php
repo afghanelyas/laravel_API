c93c3cd82b7d2581764d00c97b08b5f939070301
afghanelyas <afghan.elyas1@gmail.com>
2023-07-10 10:48:29 +0430

    validate the register and login api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 
@@ -8,16 +13,39 @@ class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(Request $request){
-       return "Login Successfull";
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+        
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json('Invalid Credentials', 401);
+        }
+        
+        $user = User::where('email' , $request->email)->first();
+        return [
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ];
+        
     }
 
-    public function register(Request $request){
-        return "Register Successfull";
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return [
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ];
     }
 
-    public function logout(Request $request){
-        return \response()->json("Logout Successfull");
+    public function logout(Request $request)
+    {
+        return \response()->json('Logout Successfull');
     }
-
 }
