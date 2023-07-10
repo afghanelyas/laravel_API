aa59c02bac18e2a59bf704666b7fee49084210d5
afghanelyas <afghan.elyas1@gmail.com>
2023-07-10 17:18:28 +0430

    make resource api in task reoute

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("/login" , [AuthController::class , "login"]);
-Route::post("/register" , [AuthController::class , "register"]);
-Route::post("/logout" , [AuthController::class , "logout"]);
+// public route
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// protected route
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('/tasks', TasksController::class);
+    
+});
